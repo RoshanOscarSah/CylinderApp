91446bf6f7bd3e332fc555c99b888646212cc9ab
RoshanOscarSah <RoshanSah729@gmail.com>
2021-05-31 23:00:10 +0545

    1.Regular Leak Sold 2.Send Recieve Done

--- app/src/main/java/com/eachut/cylinder/ui/home/HomeFragment.kt
+++ app/src/main/java/com/eachut/cylinder/ui/home/HomeFragment.kt
@@ -6,13 +6,17 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
+import android.widget.FrameLayout
 import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -29,6 +33,7 @@ class   HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -43,34 +48,34 @@ class   HomeFragment : Fragment() {
 //customer/company
         binding.tvCompany.setOnClickListener { view ->
             //for setting gravity
-//            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
-//                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
-//            )
-//            params.gravity = Gravity.RIGHT
-//            binding.ivToggleActive.setLayoutParams(params);
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = Gravity.RIGHT
+            binding.ivToggleActive.setLayoutParams(params);
 
             //for sliding animation
-            ObjectAnimator.ofFloat(binding.ivToggleActive, "translationX", 460f).apply {
-                duration = 200
-                start()
-            }
+//            ObjectAnimator.ofFloat(binding.ivToggleActive, "translationX", 460f).apply {
+//                duration = 200
+//                start()
+//            }
             binding.tvCustomerOrCompany.setText("Select Company")
             binding.tvCustomerOrCompany.setContentDescription("Change Company")
         }
 
         binding.tvCustomer.setOnClickListener { view ->
             //for setting gravity
-//            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
-//                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
-//            )
-//            params.gravity = Gravity.LEFT
-//            binding.ivToggleActive.setLayoutParams(params);
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = Gravity.LEFT
+            binding.ivToggleActive.setLayoutParams(params);
 
             //for sliding animation
-            ObjectAnimator.ofFloat(binding.ivToggleActive, "translationX", 0f).apply {
-                duration = 200
-                start()
-            }
+//            ObjectAnimator.ofFloat(binding.ivToggleActive, "translationX", 0f).apply {
+//                duration = 200
+//                start()
+//            }
             binding.tvCustomerOrCompany.setText("Select Customer")
             binding.tvCustomerOrCompany.setContentDescription("Change Customer")
         }
@@ -125,27 +130,357 @@ class   HomeFragment : Fragment() {
 //REGULAR LEAK SOLD CYLINDER
         binding.tvRegular.setOnClickListener{ view ->
             //for sliding animation
-            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 0f).apply {
-                duration = 200
-                start()
-            }
+//            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 0f).apply {
+//                duration = 200
+//                start()
+//            }
+
+            //for setting gravity
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = Gravity.LEFT
+            binding.ivRlctoggleActive.setLayoutParams(params);
+
+            //hiding other layout
+            binding.llRegular.isVisible = true
+            binding.llLeak.isVisible = false
+            binding.llSold.isVisible = false
         }
 
         binding.tvLeak.setOnClickListener{ view ->
             //for sliding animation
-            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 235f).apply {
-                duration = 200
-                start()
-            }
+//            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 235f).apply {
+//                duration = 200
+//                start()
+//            }
+
+            //for setting gravity
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = Gravity.CENTER
+            binding.ivRlctoggleActive.setLayoutParams(params);
+
+            //hiding other layout
+            binding.llRegular.isVisible = false
+            binding.llLeak.isVisible = true
+            binding.llSold.isVisible = false
+
         }
 
         binding.tvSold.setOnClickListener{ view ->
             //for sliding animation
-            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 470f).apply {
+//            ObjectAnimator.ofFloat(binding.ivRlctoggleActive, "translationX", 470f).apply {
+//                duration = 200
+//                start()
+//            }
+
+            //for setting gravity
+            val params: FrameLayout.LayoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.gravity = Gravity.RIGHT
+            binding.ivRlctoggleActive.setLayoutParams(params);
+
+            //hiding other layout
+            binding.llRegular.isVisible = false
+            binding.llLeak.isVisible = false
+            binding.llSold.isVisible = true
+
+        }
+
+        //for regular
+        binding.etGas1R.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas2R.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas3R.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas4R.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        //for leak
+        binding.etGas1L.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas2L.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas3L.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas4L.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        //for sold
+        binding.etGas1S.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas2S.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas3S.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        binding.etGas4S.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+                if (s != "") { //do your work here
+                    rslTotal()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+//Send/Receive
+        binding.llStockSend.setOnClickListener { view ->
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(binding.ivSendFangro, "translationX", 0f).apply {
                 duration = 200
                 start()
             }
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(binding.ivRecieveFangro, "translationX", -250f).apply {
+                duration = 200
+                start()
+            }
+
+
+
+            binding.ivRecieveFangro.isVisible = false
+            binding.ivSendFangro.isVisible = true
+
+            binding.llStockReceive.background.setTintList(context?.let {
+                ContextCompat.getColorStateList(
+                    it, R.color.sendreceive_fade)
+            })
+
+            binding.llStockSend.background.setTintList(context?.let {
+                ContextCompat.getColorStateList(
+                    it, R.color.sendreceive)
+            })
         }
+
+        binding.llStockReceive.setOnClickListener { view ->
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(binding.ivSendFangro, "translationX", 460f).apply {
+                duration = 200
+                start()
+            }
+
+            //for sliding animation
+            ObjectAnimator.ofFloat(binding.ivRecieveFangro, "translationX", 210f).apply {
+                duration = 200
+                start()
+            }
+
+            binding.ivRecieveFangro.isVisible = true
+            binding.ivSendFangro.isVisible = false
+
+            binding.llStockReceive.background.setTintList(context?.let {
+                ContextCompat.getColorStateList(
+                    it, R.color.sendreceive)
+            })
+
+            binding.llStockSend.background.setTintList(context?.let {
+                ContextCompat.getColorStateList(
+                    it, R.color.sendreceive_fade)
+            })
+        }
+
+
 
 
 
@@ -195,8 +530,83 @@ class   HomeFragment : Fragment() {
             binding.tvCustomerOrCompany.setText(ChangeCustomerOrCompany)
             binding.llNameSelected.isVisible = true
         })
-
-
-
     }
+
+//    for RLS total
+    fun rslTotal(){
+        //regular
+        val etGas1Rvalue = if (TextUtils.isEmpty(
+                binding.etGas1R.getText().toString()
+            )
+        ) 0 else binding.etGas1R.getText().toString().toInt()
+
+        val etGas2Rvalue = if (TextUtils.isEmpty(
+                binding.etGas2R.getText().toString()
+            )
+        ) 0 else binding.etGas2R.getText().toString().toInt()
+
+        val etGas3Rvalue = if (TextUtils.isEmpty(
+                binding.etGas3R.getText().toString()
+            )
+        ) 0 else binding.etGas3R.getText().toString().toInt()
+
+        val etGas4Rvalue = if (TextUtils.isEmpty(
+                binding.etGas4R.getText().toString()
+            )
+        ) 0 else binding.etGas4R.getText().toString().toInt()
+
+        //leak
+        val etGas1Lvalue = if (TextUtils.isEmpty(
+                binding.etGas1L.getText().toString()
+            )
+        ) 0 else binding.etGas1L.getText().toString().toInt()
+
+        val etGas2Lvalue = if (TextUtils.isEmpty(
+                binding.etGas2L.getText().toString()
+            )
+        ) 0 else binding.etGas2L.getText().toString().toInt()
+
+        val etGas3Lvalue = if (TextUtils.isEmpty(
+                binding.etGas3L.getText().toString()
+            )
+        ) 0 else binding.etGas3L.getText().toString().toInt()
+
+        val etGas4Lvalue = if (TextUtils.isEmpty(
+                binding.etGas4L.getText().toString()
+            )
+        ) 0 else binding.etGas4L.getText().toString().toInt()
+
+        //sold
+        val etGas1Svalue = if (TextUtils.isEmpty(
+                binding.etGas1S.getText().toString()
+            )
+        ) 0 else binding.etGas1S.getText().toString().toInt()
+
+        val etGas2Svalue = if (TextUtils.isEmpty(
+                binding.etGas2S.getText().toString()
+            )
+        ) 0 else binding.etGas2S.getText().toString().toInt()
+
+        val etGas3Svalue = if (TextUtils.isEmpty(
+                binding.etGas3S.getText().toString()
+            )
+        ) 0 else binding.etGas3S.getText().toString().toInt()
+
+        val etGas4Svalue = if (TextUtils.isEmpty(
+                binding.etGas4S.getText().toString()
+            )
+        ) 0 else binding.etGas4S.getText().toString().toInt()
+
+
+
+        val sumR = etGas1Rvalue + etGas2Rvalue + etGas3Rvalue +etGas4Rvalue
+        val sumL = etGas1Lvalue + etGas2Lvalue + etGas3Lvalue +etGas4Lvalue
+        val sumS = etGas1Svalue + etGas2Svalue + etGas3Svalue +etGas4Svalue
+        val sumTotal = sumR + sumL + sumS
+        binding.tvGasTotalR.setText(sumR.toString())
+        binding.tvGasTotalL.setText(sumL.toString())
+        binding.tvGasTotalS.setText(sumS.toString())
+        binding.tvGasTotal.setText(sumTotal.toString())
+    }
+
 }
